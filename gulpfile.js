b0e948e35b74e8c19ac49f9c27afdca2aacf20a1
Klajd <klajd.diko@gmail.com>
2016-06-05 09:14:02 +0200

    gulpfile config with local npm-package

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
 require('../angular-component-tasks/')(require('gulp'), {
     // Config Object
     dest: 'dist',
+    copyright: "/*! */",
     index: 'demo/index.html',
+    module: 'component.seed',
     scripts: {
         export: 'component.js',
         files: [
+            'src/*[Mm]odule.js',
+            'src/*[Cc]onfig.js',
+            'src/*[Ss]ervice.js',
+            'src/*[Ff]actory.js',
+            'src/*[Dd]irectory.js',
+            'src/*[Cc]omponent.js',
             'src/*.js',
-            'src/**/*module.js',
-            'src/**/*.service.js',
-            'src/**/*.component.js',
+            'src/**/*[Mm]odule.js',
+            'src/**/*[Cc]onfig.js',
+            'src/**/*[Ss]ervice.js',
+            'src/**/*[Ff]actory.js',
+            'src/**/*[Dd]irectory.js',
+            'src/**/*[Cc]omponent.js',
             'src/**/*.js'
         ],
         dest: 'dist'
@@ -18,6 +29,13 @@ require('../angular-component-tasks/')(require('gulp'), {
         files: [
             'src/*.css',
             'src/**/*.css'
+        ],
+        dest: 'dist'
+    },
+    templates: {
+        files: [
+            'src/*.html',
+            'src/**/*.html'
         ],
         dest: 'dist'
     },
